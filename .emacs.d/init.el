cac8b7dd35299e3e1f0a1e340597133e0c52a7b2
栗原佳輝 <yumyum0073@gmail.com>
2018-05-05 19:41:59 +0900

    Web-modeの色がおかしかった

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -401,20 +401,20 @@ FORMAT-STRING is like `format', but it can have multiple %-sequences."
 )
 (add-hook 'web-mode-hook 'web-mode-hook)
 ;; 色の設定
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(web-mode-comment-face ((t (:foreground "#D9333F"))))
- '(web-mode-css-at-rule-face ((t (:foreground "#FF7F00"))))
- '(web-mode-css-pseudo-class-face ((t (:foreground "#FF7F00"))))
- '(web-mode-css-rule-face ((t (:foreground "#A0D8EF"))))
- '(web-mode-doctype-face ((t (:foreground "#82AE46"))))
- '(web-mode-html-attr-name-face ((t (:foreground "#C97586"))))
- '(web-mode-html-attr-value-face ((t (:foreground "#82AE46"))))
- '(web-mode-html-tag-face ((t (:foreground "#E6B422" :weight bold))))
- '(web-mode-server-comment-face ((t (:foreground "#D9333F")))))
+;; (custom-set-faces
+;;  ;; custom-set-faces was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  '(web-mode-comment-face ((t (:foreground "#D9333F"))))
+;;  '(web-mode-css-at-rule-face ((t (:foreground "#FF7F00"))))
+;;  '(web-mode-css-pseudo-class-face ((t (:foreground "#FF7F00"))))
+;;  '(web-mode-css-rule-face ((t (:foreground "#A0D8EF"))))
+;;  '(web-mode-doctype-face ((t (:foreground "#82AE46"))))
+;;  '(web-mode-html-attr-name-face ((t (:foreground "#C97586"))))
+;;  '(web-mode-html-attr-value-face ((t (:foreground "#82AE46"))))
+;;  '(web-mode-html-tag-face ((t (:foreground "#E6B422" :weight bold))))
+;;  '(web-mode-server-comment-face ((t (:foreground "#D9333F")))))
 
 ;;org-modeの設定
 (setq org-startup-truncated nil)
