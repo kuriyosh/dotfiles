805dd519af6129ead560e38afa27b8ef27891978
Yoshiki Kurihara <yumyum0073@gmail.com>
2018-06-14 20:41:05 +0900

    sync

---
+++ .emacs.d/elpa/evil-ediff-20170724.1223/evil-ediff-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "evil-ediff" "20170724.1223" "Make ediff a little evil" '((evil "1.2.3")) :commit "11b779b6325aeee54749be9bf44951fdb6334d10" :url "https://github.com/justbur/evil-ediff")
