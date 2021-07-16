f1c7efa35bb8567a3fe57fb89ce8db19c0d077d0
yosyos36 <yosyos0306@gmail.com>
2021-07-16 15:48:49 +0900

    add: git setting

--- install.sh
+++ install.sh
@@ -8,28 +8,32 @@ case "$yn" in
     *) exit 1;;
 esac
 
-# # brew
-# if !(command brew -v > /dev/null 2>&1); then
-#     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-# fi
-#
-# cd $SCRIPT_DIR
-# brew bundle --file $SCRIPT_DIR/Brewfile
-#
-# # zsh
-# ln -s ~/.zprofile $SCRIPT_DIR/.zprofile
-#
-# # fish
-# mkdir ~/.config
-# ln -s ~/.config/fish $SCRIPT_DIR/fish
-#
-# # Keybind
-# mkdir -p ~/Library/KeyBindings
-# ln -s ~/Library/KeyBindings/DefaultKeyBinding.dict $SCRIPT_DIR/DefaultKeyBinding.dict
-#
-# # scripts
-# ln -s ~/.scripts $SCRIPT_DIR/.scripts
-#
-# # Emacs
-# mkdir -p ~/.emacs.d
-# ln -s ~/.emacs.d $SCRIPT_DIR/.emacs.d
+# brew
+if !(command brew -v > /dev/null 2>&1); then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+cd $SCRIPT_DIR
+brew bundle --file $SCRIPT_DIR/Brewfile
+
+# zsh
+ln -s ~/.zprofile $SCRIPT_DIR/.zprofile
+
+# fish
+mkdir ~/.config
+ln -s ~/.config/fish $SCRIPT_DIR/fish
+
+# Keybind
+mkdir -p ~/Library/KeyBindings
+ln -s ~/Library/KeyBindings/DefaultKeyBinding.dict $SCRIPT_DIR/DefaultKeyBinding.dict
+
+# scripts
+ln -s ~/.scripts $SCRIPT_DIR/.scripts
+
+# Emacs
+mkdir -p ~/.emacs.d
+ln -s ~/.emacs.d $SCRIPT_DIR/.emacs.d
+
+# git
+ln -s ~/.gitconfig $SCRIPT_DIR/.gitconfig
+ln -s ~/.gitmessage $SCRIPT_DIR/.gitmessage
