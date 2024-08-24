ea1ccbde1e7751128cb74dae55da2c6a8c23b097
kuriyosh <yosyos0306@gmail.com>
2024-08-24 11:45:14 +0900

    align emacs files

--- .emacs.d/cli.el
+++ .emacs.d/cli.el
@@ -52,17 +52,16 @@
 (if (eq system-type 'darwin)
     (progn
       (defun copy-from-osx ()
-    (shell-command-to-string "reattach-to-user-namespace pbpaste"))
+        (shell-command-to-string "reattach-to-user-namespace pbpaste"))
       (defun paste-to-osx (text &optional push)
-    (let ((process-connection-type nil))
-      (let ((proc (start-process "pbcopy" "*Messages*" "reattach-to-user-namespace" "pbcopy")))
-        (process-send-string proc text)
-        (process-send-eof proc))))
+      (let ((process-connection-type nil))
+        (let ((proc (start-process "pbcopy" "*Messages*" "reattach-to-user-namespace" "pbcopy")))
+          (process-send-string proc text)
+          (process-send-eof proc))))
       (setq interprogram-cut-function 'paste-to-osx)
       (setq interprogram-paste-function 'copy-from-osx)
     )
     (message "This platform is not mac"))
-
 
 ;; ;; ===============================================================
 ;; ;; Key Setting
