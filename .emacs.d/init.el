7d6663bfbc5a61305b7b5ba8c1eeb0eb98500360
kuriyosh <yosyos0306@gmail.com>
2025-02-08 15:15:54 +0900

    add orbstack

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -9,7 +9,8 @@
 
 (require 'package)
 
-; list the repositories containing them
+;; list the repositories containing them
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (add-to-list 'package-archives '("elpa" . "http://tromey.com/elpa/") t)
 (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/") t)
